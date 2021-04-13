3e5627fe5b3ff7dac870d146a1c4dfea1a3afb84
ammarnasr <ammarnasraza@gmail.com>
2021-04-13 15:19:29 +0200

    Customer, Driver and Order Details
    General Layout of pages that show single entry from customers , drivers and orders

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import Admin from "layouts/Admin.js";
 import Auth from "layouts/Auth.js";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Landing from "views/Landing.js";
-import Profile from "views/Profile.js";
+import CustomerDetails from "views/CustomerDetails.js";
+import OrderDetails from "views/OrderDetails.js";
+import DriverDetails from "views/DriverDetails.js";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Switch>
                 {/* add routes with layouts */}
-                <PrivateRoute path="/admin" component={Admin} />
+                <Route path="/admin" component={Admin} />
                 <Route path="/auth" component={Auth} />
                 {/* add routes without layouts */}
-                <PrivateRoute path="/profile" exact component={Profile} />
+                <Route path="/customer" exact component={CustomerDetails} />
+                <Route path="/order" exact component={OrderDetails} />
+                <Route path="/driver" exact component={DriverDetails} />
                 <Route path="/landing" exact component={Landing} />
                 {/* add redirect for first page */}
                 <Redirect from="*" to="/admin" />
