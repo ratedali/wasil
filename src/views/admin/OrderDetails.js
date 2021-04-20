c98851502576437a6f07abbf0849f2fbb0042615
Ammar <ammarnasraza@gamil.com>
2021-04-20 11:28:43 +0200

    Link Details to order assingment and approve

--- src/views/admin/OrderDetails.js
+++ src/views/admin/OrderDetails.js
@@ -32,12 +32,7 @@ export default function OrderDetails() {
             <div className="w-full lg:w-6/12 px-4 lg:order-2 lg:text-right lg:self-center">
               <div className="py-6 px-3 mt-32 sm:mt-0">
                 <Link to={`/admin/approve/${order.driverId}`}>
-                  <button
-                    className="bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    Edit
-                  </button>
+                  <EditButton status={order.status}/>
                 </Link>
               </div>
             </div>
@@ -102,4 +97,45 @@ export default function OrderDetails() {
       </div>
     </>
   );
+}
+
+function EditButton({status}) {
+  const { id } = useParams();
+  if (status === "new") {
+    return(
+      <Link to={`/admin/orders/approve/${id}`}>
+      <button
+        className="bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        >
+        Approve
+      </button>
+    </Link>
+        )
+  }
+
+  if (status === "confirmed") {
+    return(
+      <Link to={`/admin/orders/assign/${id}`}>
+      <button
+        className="bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        >
+        Assign
+      </button>
+    </Link>
+        )
+  }
+
+    return(
+      <Link to={`/admin/orders/edit/${id}`}>
+      <button
+        className="bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        >
+        Edit
+      </button>
+    </Link>
+        )
+
 }
